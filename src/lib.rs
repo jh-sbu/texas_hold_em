3e02f4257b6162a85ccec7cb53cedbc47dea1956
Joseph.Hess@Stonybrook.edu <Joseph.Hess2@gmail.com>
2026-04-08 12:40:27 -0400

    MCTS Skeleton
    - Added MCTS skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod deck;
+mod mcts;
 mod player;
 mod poker;
---
+++ src/mcts.rs
@@ -0,0 +1,19 @@
+struct Mcts {}
+
+impl Mcts {
+    pub fn new() -> Self {
+        todo!();
+    }
+
+    pub fn selection(&self) -> Self {
+        todo!();
+    }
+
+    pub fn expansion(&self) -> Self {
+        todo!();
+    }
+
+    pub fn backprop(&self) -> Self {
+        todo!();
+    }
+}
